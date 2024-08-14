db9c1152ff4938352cb261e10fdadd26875c1226
Kanueven <raelrakeli@gmail.com>
2024-08-14 22:27:33 +0300

    Initial commit

---
+++ app/src/main/java/com/example/racetracker/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.racetracker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.racetracker.presentation.RaceTrackerApp
+import com.example.racetracker.ui.theme.RaceTrackerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RaceTrackerTheme {
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    RaceTrackerApp()
+                }
+
+            }
+        }
+    }
+}
+@Preview
+@Composable
+fun RacePreview(){
+    RaceTrackerApp()
+}
+
---
+++ app/src/main/java/com/example/racetracker/presentation/RaceParticipant.kt
@@ -0,0 +1,40 @@
+package com.example.racetracker.presentation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import com.example.racetracker.presentation.RaceParticipant
+
+class RaceParticipant(
+    val name: String,
+    val maxProgress: Int = 100 ,
+    val progressDelayMillis: Long = 500L,
+    private  val progressIncrement: Int=1,
+    private val  initialProgress : Int =0
+){
+    init{
+        require(maxProgress > 0){"maxProgress = $maxProgress; must be > 0"}
+        require(progressIncrement > 0){"progressIncrement = $progressIncrement; must be > 0"}
+    }
+    /**
+     * Indicates the race participant's current progress
+     */
+    var currentProgress by mutableStateOf(initialProgress)
+    private set
+
+    /**
+     * Regardless of the value of [initialProgress] the reset function will reset the
+     * [currentProgress] to 0
+     */
+    fun reset(){
+        currentProgress = 0
+    }
+}
+/**
+ * The Linear progress indicator expects progress value in the range of 0-1. This property
+ * calculate the progress factor to satisfy the indicator requirements.
+ * This property calculates the participant's progress as a percentage of the maximum progress.
+ */
+
+val RaceParticipant.progressFactor: Float
+    get() = currentProgress/maxProgress.toFloat()
